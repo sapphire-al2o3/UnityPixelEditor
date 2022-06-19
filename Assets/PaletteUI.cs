8031d657f3d19a3895a1a70626f1bc8547ae9643
sapphire-al2o3 <t_telkun@hotmail.com>
2022-06-19 11:54:17 +0900

    パレットボタンの色を自動で設定する

---
+++ Assets/PaletteUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PaletteUI : MonoBehaviour
+{
+	[SerializeField]
+	UnityPixelEditor editor;
+
+	Image[] images;
+
+	private void Start()
+	{
+		images = new Image[transform.childCount];
+		var palette = editor.GetPalette();
+		for (int i = 0; i < palette.Length && i < images.Length; i++)
+		{
+			images[i] = transform.GetChild(i).GetComponent<Image>();
+			images[i].color = palette[i];
+		}
+	}  
+}
