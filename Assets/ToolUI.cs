b930cea86502505e2c2e3500d1520f2b3cb6b3bb
sapphire-al2o3 <t_telkun@hotmail.com>
2022-07-03 12:33:09 +0900

    ツールボタンの選択状態の見た目設定

---
+++ Assets/ToolUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolUI : MonoBehaviour
+{
+	//[SerializeField]
+	//UnityPixelEditor editor;
+
+	[SerializeField]
+	Button[] toolButtons;
+
+	[SerializeField]
+	Color normalColor;
+
+	[SerializeField]
+	Color selectedColor;
+
+	private void Awake()
+	{
+		foreach(var button in toolButtons)
+		{
+			var colors = button.colors;
+			colors.normalColor = normalColor;
+		}
+
+		var selectedButton = toolButtons[0].colors;
+		selectedButton.normalColor = selectedColor;
+		selectedButton.selectedColor = selectedColor;
+		toolButtons[0].colors = selectedButton;
+	}
+
+	public void Select(int index)
+	{
+		for (int i = 0; i < toolButtons.Length; i++)
+		{
+			var colors = toolButtons[i].colors;
+			colors.normalColor = i == index ? selectedColor : normalColor;
+			colors.selectedColor = i == index ? selectedColor : normalColor;
+			toolButtons[i].colors = colors;
+		}
+	}
+}
