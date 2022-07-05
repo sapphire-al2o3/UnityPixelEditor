866081c8cc641baffd46d99ff84ed66c0d799791
sapphire-al2o3 <t_telkun@hotmail.com>
2022-07-05 23:15:41 +0900

    パレットを切り替える機能作成

--- Assets/PaletteUI.cs
+++ Assets/PaletteUI.cs
@@ -10,6 +10,9 @@ public class PaletteUI : MonoBehaviour
 
 	Image[] images;
 
+	int index = 0;
+	List<Color32> colorSet = new List<Color32>();
+
 	private void Start()
 	{
 		images = new Image[transform.childCount];
@@ -19,11 +22,24 @@ public class PaletteUI : MonoBehaviour
 			images[i] = transform.GetChild(i).GetComponent<Image>();
 			images[i].color = palette[i];
 		}
+
+		colorSet.Add(new Color32(0, 0xFF, 0, 0xFF));
+		colorSet.Add(new Color32(0xFF, 0, 0xFF, 0xFF));
+		colorSet.Add(new Color32(0, 0, 0xFF, 0xFF));
 	}
 
 	public void ChangePalette(int dir)
 	{
+		index = (index + dir + colorSet.Count) % colorSet.Count;
 		var palette = editor.GetPalette();
+		palette[0] = colorSet[index];
+
+		for (int i = 0; i < palette.Length && i < images.Length; i++)
+		{
+			images[i] = transform.GetChild(i).GetComponent<Image>();
+			images[i].color = palette[i];
+		}
+		editor.Refresh();
 	}
 
 	void CreatePalette()
