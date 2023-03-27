5d9614e845ad3814f7def5dadc06bb0d6f685bf8
sapphire-al2o3 <t_telkun@hotmail.com>
2023-03-27 23:41:18 +0900

    エディタから戻るときに画像データをセットする

--- Assets/SceneTransition.cs
+++ Assets/SceneTransition.cs
@@ -8,8 +8,18 @@ public class SceneTransition : MonoBehaviour
     [SerializeField]
     string scene;
 
+    [SerializeField]
+    PixelImageData image;
+
+    [SerializeField]
+    UnityPixelEditor editor;
+
     public void Run()
     {
+        image.width = 16;
+        image.height = 16;
+        image.indexData = editor.GetIndex();
+        image.paletteData = editor.GetPalette();
         SceneManager.LoadScene(scene);
     }
 }
