ad18e1ad1819d17eaefceececed2ca7c010566c4
sapphire-al2o3 <t_telkun@hotmail.com>
2023-03-28 23:45:53 +0900

    テクスチャをキャッシュする

--- Assets/Samples/EditBox.cs
+++ Assets/Samples/EditBox.cs
@@ -13,11 +13,36 @@ public class EditBox : MonoBehaviour
     [SerializeField]
     GameObject[] boxs;
 
+    static Texture2D[] cacheTextures = null;
+    static PixelImageData[] cacheImages = null;
+
     void Awake()
     {
+        if (cacheTextures != null)
+        {
+            cacheTextures = new Texture2D[boxs.Length];
+        }
+
+        if (cacheImages != null)
+        {
+            cacheImages = new PixelImageData[boxs.Length];
+            for (int i = 0; i < cacheImages.Length; i++)
+            {
+                cacheImages[i] = new PixelImageData();
+            }
+        }
+
         if (selectedBox >= 0)
         {
             boxs[selectedBox].GetComponent<PixelImageSetter>().Setup(tempImage.indexData, tempImage.paletteData);
+        }
+
+        for (int i = 0; i < boxs.Length; i++)
+        {
+            if (cacheTextures[i] != null)
+            {
+                boxs[i].GetComponent<Renderer>().material.mainTexture = cacheTextures[i];
+            }
         }
     }
 
