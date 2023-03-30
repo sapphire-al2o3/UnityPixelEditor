28b521af34f3eabd3dac9c037671b8c7653bfde1
sapphire-al2o3 <t_telkun@hotmail.com>
2023-03-30 23:53:22 +0900

    コピーメソッド追加

--- Assets/PixelImageData.cs
+++ Assets/PixelImageData.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 [CreateAssetMenu()]
@@ -9,4 +10,23 @@ public class PixelImageData : ScriptableObject
     public int height;
     public byte[] indexData;
     public Color32[] paletteData;
+
+    public void Copy(PixelImageData src)
+    {
+        width = src.width;
+        height = src.height;
+        if (indexData == null || indexData.Length != src.indexData.Length)
+        {
+            indexData = new byte[src.indexData.Length];
+        }
+
+        Array.Copy(src.indexData, indexData, indexData.Length);
+
+        if (paletteData == null || paletteData.Length != src.paletteData.Length)
+        {
+            paletteData = new Color32[src.paletteData.Length];
+        }
+
+        Array.Copy(src.paletteData, paletteData, paletteData.Length);
+    }
 }
